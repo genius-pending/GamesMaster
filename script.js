b98aa2b5e01c314ee23d1c306661875f39797606
Shivey Singh <shivey_khangura@hotmail.com>
2022-03-19 21:33:11 +0000

    added function to display api call on screen

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ var simpleModal = document.getElementById('simplemodal');
 var closeBtn = document.getElementById('closeBtn');
 var card = document.getElementById('card');
 var searchButton = document.getElementById('searchfield');
+var gamecardBtn = document.getElementById('gamecardimg');
+var overview = document.getElementById('overview');
 var API_KEY = '2b5025a6e62447588a8adb3f3c732a71'
-var Base_URL = 'https://api.rawg.io/api/games?key=' + API_KEY
+var Base_URL = `https://api.rawg.io/api/games?key=` + API_KEY  + `&search=`
+var closeSynopsisBtn = document.getElementById('overviewcloseBtn');
 
 //Modal function to open and close the modal//
 modalBtn.addEventListener('click', openModal);
@@ -14,6 +17,7 @@ function openModal() {
   simpleModal.style.display = 'block';
   document.body.style.backgroundImage = "URL('gamesmaster1.jpeg')";
   card.style.display = 'none';
+  overview.style.display = 'none';
 }
 closeBtn.addEventListener('click', closeModal);
 function closeModal() {
@@ -21,47 +25,76 @@ function closeModal() {
   document.body.style.backgroundImage = "URL('gamesmaster1.jpeg')";
 }
 
-//function to get user input and call api
-searchButton.addEventListener('click', searchGame);
-function searchGame(event) {
-  event.preventDefault()
-
-  var searchValue = document.getElementById('input');
-  simpleModal.style.display = 'none';
-  card.style.display = 'block';
-  document.body.style.backgroundImage = 'none';
-  console.log('value:', searchValue.value);
-
-  fetch(Base_URL + '&search=' + searchValue.value)
-    .then((res) => res.json())
-    .then((data) => {
-      const games = data.results
-      console.log('Data', games);
-    })
-    .catch((error) => {
-      console.log('Error:', error);
-    });
-  console.log('Value:', searchValue.value);
+gamecardBtn.addEventListener('click', openSynopsis);
+function openSynopsis() {
+  overview.style.display = 'block';
 }
 
-function getGames(games) {
-  return games.map((game) => {
-    return `
-    <img src=${games.background_image} class="card-img-top" alt="...">
-    `
-  });
+closeSynopsisBtn.addEventListener('click', closeSynopsis);
+function closeSynopsis() {
+  overview.style.display = 'none';
+}
+
+function getGames(url){ 
+  var searchValue = document.getElementById('input');
+  fetch(url+searchValue.value).then(res => res.json()).then(data => {
+    showGames(data.results)
+    console.log(data.results)
+    
+  })
+}
   
+  function showGames(data){
+
+    card.innerHTML = "";
+    
+    data.forEach(game => {
+      const{name,released,background_image,metacritic} = game
+      const gameEl = document.createElement('div');
+      gameEl.classList.add('cardonclick')
+      gameEl.innerHTML = ` 
+      <img class="card-img-top" src=${background_image} id="gamecardimg" alt="Card image cap">
+      <div class="card-body">
+      <h5 class="card-title">${name}</h5>
+      </div>
+      <ul class="list-group list-group-flush">
+      <li class="list-group-item">Available On:</li>
+      <li class="list-group-item">Released On: ${released}</li>
+      <li class="list-group-item .${getColour(metacritic)}">Rating: ${metacritic}</li>
+      </ul>
+      <div class="card-body">
+      <a href="#" class="card-link">Reviews</a>
+      </div>
+      </div>`
+      
+      card.appendChild(gameEl)
+      
+    });
+    
+  }
   
+  function clearsearch(){
+    simpleModal.style.display = 'none';
+    card.style.display = 'block';
+    document.body.style.backgroundImage = 'none';
+  }
   
-function showGames(games) {
-    const gameElement = document.createElement('div');
-    gameElement.setAttribute('class', 'card');
+  function getColour(vote){
+    if(vote>= 80){
+      return 'green'
+    }else if (vote>=50){
+      return 'orange'
+    }else{
+      return 'red'}
+    }
+  
+  //function to get user input and call api
+  searchButton.onclick = function(e){
+    
+    e.preventDefault();
+    clearsearch();
+    getGames(Base_URL);
 
-    const gameTemplate =
+  }
 
-      `<div class="card" style="width: 18rem;" id="card"></div>`
-      getGames(games)
-
-
-  } 
-;}
+  
