3c9e07f38e41969b492b7aec2c9f73957a5f5181
Shivey Singh <shivey_khangura@hotmail.com>
2022-03-23 17:16:29 +0000

    fixed some bugs
    i've got the search function to refresh on each api call and display the new results as a fresh search rather then being appended to the bottom of the previous search also sorted out the cards too look more consistent just need to sort out the images in the cards now so that they are all equal in size .

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-
 //variables to hookup HTML features//
 var modalBtn = document.getElementById('modalBtn');
 var simpleModal = document.getElementById('simplemodal');
 var closeBtn = document.getElementById('closeBtn');
-var card = document.getElementById('card');
+var cardcontainer = document.getElementById('cardcontainer');
 var searchButton = document.getElementById('searchfield');
 var gamecardBtn = document.getElementById('gamecardimg');
 var overview = document.getElementById('overview');
 var API_KEY = '2b5025a6e62447588a8adb3f3c732a71'
 var Base_URL = `https://api.rawg.io/api/games?key=` + API_KEY  + `&search=`
 var closeSynopsisBtn = document.getElementById('overviewcloseBtn');
+var card = document.getElementById('card');
+var footer = document.getElementById('footer');
 
 //Modal function to open and close the modal//
 modalBtn.addEventListener('click', openModal);
@@ -18,6 +19,8 @@ function openModal() {
   document.body.style.backgroundImage = "URL('gamesmaster1.jpeg')";
   card.style.display = 'none';
   overview.style.display = 'none';
+  cardcontainer.style.display = 'none';
+  footer.style.display ='none';
 }
 closeBtn.addEventListener('click', closeModal);
 function closeModal() {
@@ -25,6 +28,7 @@ function closeModal() {
   document.body.style.backgroundImage = "URL('gamesmaster1.jpeg')";
 }
 
+//Show description of game function
 gamecardBtn.addEventListener('click', openSynopsis);
 function openSynopsis() {
   overview.style.display = 'block';
@@ -34,16 +38,17 @@ closeSynopsisBtn.addEventListener('click', closeSynopsis);
 function closeSynopsis() {
   overview.style.display = 'none';
 }
-
+//calls api to get data in json
 function getGames(url){ 
   var searchValue = document.getElementById('input');
   fetch(url+searchValue.value).then(res => res.json()).then(data => {
     showGames(data.results)
     console.log(data.results)
     
+    
   })
 }
-  
+ //displays results from api call on the screen 
   function showGames(data){
 
     card.innerHTML = "";
@@ -51,8 +56,9 @@ function getGames(url){
     data.forEach(game => {
       const{name,released,background_image,metacritic} = game
       const gameEl = document.createElement('div');
-      gameEl.classList.add('cardonclick')
+      gameEl.classList.add('cardclick')
       gameEl.innerHTML = ` 
+      
       <img class="card-img-top" src=${background_image} id="gamecardimg" alt="Card image cap">
       <div class="card-body">
       <h5 class="card-title">${name}</h5>
@@ -65,19 +71,26 @@ function getGames(url){
       <div class="card-body">
       <a href="#" class="card-link">Reviews</a>
       </div>
-      </div>`
+      </div>
+
+      `
       
-      card.appendChild(gameEl)
+      cardcontainer.appendChild(gameEl)
       
     });
     
   }
   
-  function clearsearch(){
+  function searchdisplay(){
     simpleModal.style.display = 'none';
-    card.style.display = 'block';
     document.body.style.backgroundImage = 'none';
+    footer.style.display='block';
+    
   }
+
+
+
+
   
   function getColour(vote){
     if(vote>= 80){
@@ -92,8 +105,12 @@ function getGames(url){
   searchButton.onclick = function(e){
     
     e.preventDefault();
-    clearsearch();
+    cardcontainer.innerHTML="";
+    searchdisplay();
     getGames(Base_URL);
+    
+    
+    
 
   }
 
