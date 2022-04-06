529c27943cff91f4c055b32f7b1d54cfc9dfeb78
Shivey Singh <shivey_khangura@hotmail.com>
2022-04-06 22:27:00 +0100

    formatted date on api call and styled cards added footer

--- script.js
+++ script.js
@@ -6,11 +6,15 @@ var cardcontainer = document.getElementById('cardcontainer');
 var searchButton = document.getElementById('searchfield');
 var gamecardBtn = document.getElementById('gamecardimg');
 var overview = document.getElementById('overview');
-var API_KEY = '2b5025a6e62447588a8adb3f3c732a71'
-var Base_URL = `https://api.rawg.io/api/games?key=` + API_KEY  + `&search=`
+var API_KEY = '?key=2b5025a6e62447588a8adb3f3c732a71'
+var Base_URL = `https://api.rawg.io/api/games` + API_KEY + `&search=`
+var Description_URL = `https://api.rawg.io/api/games/`
 var closeSynopsisBtn = document.getElementById('overviewcloseBtn');
 var card = document.getElementById('card');
 var footer = document.getElementById('footer');
+var plotcontainer = document.getElementById('plotcontainer');
+
+
 
 //Modal function to open and close the modal//
 modalBtn.addEventListener('click', openModal);
@@ -20,8 +24,9 @@ function openModal() {
   card.style.display = 'none';
   overview.style.display = 'none';
   cardcontainer.style.display = 'none';
-  footer.style.display ='none';
-  
+  footer.style.display = 'none';
+
+
 }
 closeBtn.addEventListener('click', closeModal);
 function closeModal() {
@@ -29,44 +34,47 @@ function closeModal() {
   document.body.style.backgroundImage = "URL('gamesmaster1.jpeg')";
 }
 
-//Show description of game function
-gamecardBtn.addEventListener('click', openSynopsis);
-function openSynopsis() {
-  overview.style.display = 'block';
-}
+
 
 closeSynopsisBtn.addEventListener('click', closeSynopsis);
 function closeSynopsis() {
   overview.style.display = 'none';
 }
+
 //calls api to get data in json
-function getGames(url){ 
+function getGames(url) {
   var searchValue = document.getElementById('input');
-  fetch(url+searchValue.value).then(res => res.json()).then(data => {
+  fetch(url + searchValue.value).then(res => res.json()).then(data => {
     showGames(data.results)
     console.log(data.results)
-    
-    
+
   })
 }
- //displays results from api call on the screen 
-  function showGames(data){
 
-    cardcontainer.innerHTML = "";
-    
-    data.forEach(game => {
-      const{name,released,background_image,metacritic} = game
-      const gameEl = document.createElement('div');
-      gameEl.classList.add('cardclick')
-      gameEl.innerHTML = ` 
+//displays results from api call on the screen 
+function showGames(data) {
+
+  cardcontainer.innerHTML = "";
+
+  data.forEach(game => {
+    const { name, released, background_image, metacritic, id } = game
+    const gameEl = document.createElement('div');
+    gameEl.classList.add('cardclick');
+    let day = new Date(released).getDate();
+    let month = new Date(released).getMonth() + 1;
+    let year = new Date(released).getFullYear();
+    let newdate = `${day}/${month}/${year}`
+    console.log('day:' + day)
+
+    gameEl.innerHTML = ` 
       
       <img class="card-img-top" src=${background_image} id="gamecardimg" alt="Card image cap">
       <div class="card-body">
-      <h5 class="card-title">${name}</h5>
+      <h5 class="card-title" id="gamename">${name}</h5>
       </div>
       <ul class="list-group list-group-flush">
       <li class="list-group-item">Available On:</li>
-      <li class="list-group-item">Released On: ${released}</li>
+      <li class="list-group-item">Released On: ${newdate}</li>
       <li class="list-group-item .${getColour(metacritic)}">Rating: ${metacritic}</li>
       </ul>
       <div class="card-body">
@@ -75,45 +83,58 @@ function getGames(url){
       </div>
 
       `
-      
-      cardcontainer.appendChild(gameEl)
-      
-    });
-    
-  }
-  
-  function searchdisplay(){
-    simpleModal.style.display = 'none';
-    document.body.style.backgroundImage = 'none';
-    footer.style.display='block';
-    cardcontainer.style.display = 'block';
-    
-    
-  }
+    cardcontainer.appendChild(gameEl)
+
+    var description = document.getElementsByClassName("card-img-top")
+    for (var i = 0; i < description.length; i++) {
+      description[i].addEventListener('click', openDescription);
+
+      function openDescription() {
 
 
+        console.log(id);
 
-
-  
-  function getColour(vote){
-    if(vote>= 80){
-      return 'green'
-    }else if (vote>=50){
-      return 'orange'
-    }else{
-      return 'red'}
+        }
     }
-  
-  //function to get user input and call api
-  searchButton.onclick = function(e){
-    
-    e.preventDefault();
-    searchdisplay();
-    getGames(Base_URL);
-    
-    
     
 
+  });
+
+
+
+
+
+}
+function searchRefresh() {
+  simpleModal.style.display = 'none';
+  document.body.style.backgroundImage = 'none';
+  footer.style.display = 'block';
+  cardcontainer.style.display = 'block';
+
+
+}
+
+
+function getColour(vote) {
+  if (vote >= 80) {
+    return 'green'
+  } else if (vote >= 50) {
+    return 'orange'
+  } else {
+    return 'red'
   }
+}
 
-  
+//function to get user input and call api
+searchButton.onclick = function (e) {
+
+  e.preventDefault();
+  searchRefresh();
+  getGames(Base_URL);
+}
+
+
+
+
+
+
